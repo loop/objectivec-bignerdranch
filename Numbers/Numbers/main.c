fa41b3db348c57bc1d49c296f3ddbc1edfde64cf
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 15:21:54 -0700

    operations

--- Numbers/Numbers/main.c
+++ Numbers/Numbers/main.c
@@ -7,17 +7,15 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[])
 {
 
-    unsigned long x = 255;
-    printf("x is %lu.\n", x);
-    
-    // Octal and hex already assumed the number was assigned
-    printf("In octal, x is %lo.\n", x);
-    printf("In hexadecimal, x is %lx.\n", x);
-    
+    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    printf("11 / 3 = %d remainder of %d\n", 11 /3, 11 % 3);
+    printf("11 / 3.0 = %f\n", 11/(float)3);
+    printf("The absolute value of -5 is %d\n", abs(5));
     return 0;
 }
 
