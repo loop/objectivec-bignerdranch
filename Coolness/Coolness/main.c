226f3bfbc7c57443c758ce20622e139294aab6b9
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 16:57:35 -0700

    while loop

---
+++ Coolness/Coolness/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by Yogesh Nagarur on 01/06/2014.
+//  Copyright (c) 2014 Yogesh Nagarur. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i = 0;
+    while (i < 12) {
+        printf("%d. Aaron is Cool\n", i);
+        i++;
+    }
+    
+    return 0;
+}
+
