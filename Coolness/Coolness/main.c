4a220c49f504f67abc681f94dd4715e4c996522d
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 16:59:18 -0700

    for loop

--- Coolness/Coolness/main.c
+++ Coolness/Coolness/main.c
@@ -10,10 +10,8 @@
 
 int main(int argc, const char * argv[])
 {
-    int i = 0;
-    while (i < 12) {
-        printf("%d. Aaron is Cool\n", i);
-        i++;
+    for (int i = 0; i < 12; i++) {
+        printf("%d. Aaron is cool\n", i);
     }
     
     return 0;
