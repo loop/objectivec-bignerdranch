b5853311a90a80687658f81089f1739a49fe68f9
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 15:26:28 -0700

    floating-point numbers

--- Numbers/Numbers/main.c
+++ Numbers/Numbers/main.c
@@ -12,10 +12,9 @@
 int main(int argc, const char * argv[])
 {
 
-    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
-    printf("11 / 3 = %d remainder of %d\n", 11 /3, 11 % 3);
-    printf("11 / 3.0 = %f\n", 11/(float)3);
-    printf("The absolute value of -5 is %d\n", abs(5));
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
     return 0;
 }
 
