72d96beb83c14ed49400ac3e0f07d5f841df4c14
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 17:07:19 -0700

    do-while loop

--- Coolness/Coolness/main.c
+++ Coolness/Coolness/main.c
@@ -10,18 +10,11 @@
 
 int main(int argc, const char * argv[])
 {
-    int i;
-    for (i = 0; i < 12; i++) {
-        if (i % 3 == 0) {
-            continue;
-        }
-        printf("Checking i = %d\n", i);
-        if (i + 90 == i * i) {
-            break;
-        }
-    }
-    printf("The answer is %d.\n", i);
-    
+    int i = 0;
+    do {
+        printf("%d. Aaron is cool\n", i);
+        i++;
+    } while (i < 13);
     return 0;
 }
 
