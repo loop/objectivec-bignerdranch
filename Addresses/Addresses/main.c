68afc1ed07149880f2fb593b3123c528093af041
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 19:29:24 -0700

    challenge for addresses

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -11,13 +11,17 @@
 int main(int argc, const char * argv[])
 {
 
-    int i = 17;
-    int *addressOfI = &i;
-    printf("i stores its value at %p\n", addressOfI);
-    *addressOfI = 89;
-    printf("Now i is %d\n", i);
-    printf("An int is %zu bytes\n", sizeof(i));
-    printf("A pointer is %zu bytes\n", sizeof(addressOfI));
+    printf("A float consumes %zu bytes\n", sizeof(float));
+    printf("A int consumes %zu bytes\n", sizeof(int));
+    printf("A short consumes %zu bytes\n", sizeof(short));
+    // A short stores 1 bit for sign
+    // 2 * 8 = 16 bits
+    // 16 - 1 = 15 bits
+    // 2^15 = 32768
+    // -32768 to +32768
+    // unsigned short can hold up to 65536
+    printf("A char consumes %zu bytes\n", sizeof(char));
+
     return 0;
 }
 
