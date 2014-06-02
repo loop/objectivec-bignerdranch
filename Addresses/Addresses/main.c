11886b93db46b83ddfb3c98587a264cd09ad2c2f
Yogesh Nagarur <yogeshnagarur@gmail.com>
2014-06-01 19:22:10 -0700

    size of using no arguments

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -14,8 +14,10 @@ int main(int argc, const char * argv[])
     int i = 17;
     int *addressOfI = &i;
     printf("i stores its value at %p\n", addressOfI);
-    printf("this function starts at %p\n", main);
-    printf("the int stores at addressOfU us %d\n", *addressOfI);
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(int *));
     return 0;
 }
 
